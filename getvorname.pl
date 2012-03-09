deeb1b4d01edb9d674ca1ba532e8b50b1704b696
Heinrich Schmidt <kt003@ktrask.de>
2012-03-09 21:06:08 +0100

    Hinzufügen eines Skriptes um Überweisungsdaten, die als csv vorliegen, zeilenweise nach Mitgliedern zu durchsuchen. Aufruf: perl readkontocsv.pl mitgliedsdaten.csv überweisungsdaten.csv

--- getvorname.pl
+++ getvorname.pl
@@ -2,6 +2,7 @@
 
 use v5.10.1;
 
+#es weden listen mit allen bekannten vornamen und nachnamen und mitgliedsnummern erstellt
 $_ = `perl filtergeneric.pl $ARGV[0] Vorname . Vorname`;
 s/"//g;
 chomp();
@@ -15,13 +16,21 @@ s/"//g;
 chomp();
 @Nummern = split/;/;
 
-$_ = `cat $ARGV[1]`;
 
+#der parameter $ARGV[1] gibt entweder die zu öffnende/durchsuchende inputdatei an, oder sie enthält den string der durchsucht werden soll
+if(-e $ARGV[1]) {
+	$_ = `cat $ARGV[1]`;
+}
+else {
+	$_ = $ARGV[1];
+}
+
+#nachdem alle daten vorliegen durchsucht die folgende schleife die daten nach vornamen und nachnamen sowie möglicherweise vorhandenen mitgliedsnummern
 for($i = 0; $i < scalar @Nachnamen; $i++)
 {
-	if((m/$Vornamen[$i]/)and($Vornamen[$i] ne ""))
+	if((m/$Vornamen[$i]/i)and($Vornamen[$i] ne ""))
 	{
-		if((m/$Nachnamen[$i]/)and($Nachnamen[$i] ne ""))
+		if((m/$Nachnamen[$i]/i)and($Nachnamen[$i] ne ""))
 		{
 			say $Vornamen[$i] . " " . $Nachnamen[$i];
 		}
